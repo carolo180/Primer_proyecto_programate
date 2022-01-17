98423683577884fd5bec0bfd12a81a1582779b71
carolo180 <caponeris1@gmail.com>
2022-01-17 13:19:37 -0500

    Agrego mejora de gradiente a los iconos, corrijo el enlace de redireccionamiento a las secciones

--- js/project.js
+++ js/project.js
@@ -1,15 +1,13 @@
 //Eventos target item de navegacion
 
+let learning = document.getElementById('learning')
+let begin = document.getElementById('begin')
+let notes = document.getElementById('notes')
+
 notes.addEventListener('click', e =>{
     e.preventDefault()
- 
-   let notes = document.getElementById('notes')
-   let learning = document.getElementById('learning')
-   let begin = document.getElementById('begin')
-  // let second_section = document.getElementById('second_section')
-   //window.location.href=("second_section")
-
-   window.location.replace("file:///home/mint/Documentos/Primer_proyecto/FIrst_proyect.html#second_section")
+   
+   window.location.replace("#second_section")
    notes.setAttribute('class','new_menu')
    learning.setAttribute('class','menu')
    begin.setAttribute('class','menu')
@@ -17,11 +15,8 @@ notes.addEventListener('click', e =>{
 
 begin.addEventListener('click', e =>{
     e.preventDefault()
- 
-   let begin = document.getElementById('begin')
-   let learning = document.getElementById('learning')
-  
-   window.location.replace("file:///home/mint/Documentos/Primer_proyecto/FIrst_proyect.html#main_section")
+   
+   window.location.replace("#main_section")
    begin.setAttribute('class','new_menu')
    notes.setAttribute('class','menu')
    learning.setAttribute('class','menu')
@@ -30,10 +25,7 @@ begin.addEventListener('click', e =>{
 learning.addEventListener('click', e =>{
     e.preventDefault()
  
-   let learning = document.getElementById('learning')
-   let begin = document.getElementById('begin')
-
-   window.location.replace("file:///home/mint/Documentos/Primer_proyecto/FIrst_proyect.html#third_section")
+   window.location.replace("#third_section")
    learning.setAttribute('class','new_menu')
    begin.setAttribute('class','menu')
    notes.setAttribute('class','menu')
